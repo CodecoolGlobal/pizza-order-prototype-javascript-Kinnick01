b8ed0c849d786658a6112ed986351711fc0ffbaa
Kraus Adam <krausadam@Kraus-MacBook-Pro.local>
2022-08-18 10:11:16 +0200

    adding everythings

--- static/script.js
+++ static/script.js
@@ -1,18 +1,9 @@
 document
 .getElementById("menu-show-allergens")
-.addEventListener("click", showAllergens() =>{
-        
-});
+.addEventListener("click", showAllergens)
 
 
 function showAllergens() {
-    pizzasList.allergens.forEach(function(allergen) {
-        <div>
-          <label for="allergen.id">allergen.name</label>
-        <input type="checkbox" id="allergen.id">
-        </div>
-      })
-   }
-
+    
 
 console.log('hello')
