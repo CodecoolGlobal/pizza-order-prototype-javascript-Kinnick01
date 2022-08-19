9b3f81a7ce068d3543f4d645698fcafeb4152273
Marton Benjamin Szak <105061239+martonszak@users.noreply.github.com>
2022-08-19 14:26:19 +0200

    Fix: total price summary, pizza numbers

--- server.js
+++ server.js
@@ -16,60 +16,64 @@ const allergensRouter = require("./routers/allergens")
 app.use("/api/allergens", allergensRouter)
 
 app.get('/', function (req, res) {
-    let package = JSON.parse(fs.readFileSync("./menu.json"));
-    res.render("index", { welcome: `?` })
+    res.render("index");
 });
 app.get('/pizza/list', function (req, res) {
     let package = JSON.parse(fs.readFileSync("./menu.json"));
     res.render("menu", { pizzasList: package })
 })
-app.get('/myorder', function (req, res) {
-    let package = JSON.parse(fs.readFileSync("./order.json"));
-    res.render("order", { orderList: package.orders });
-})
-app.post("/myorder", function (req, res) {
 
+app.get("/myorder", function (req, res) {
+    let package = JSON.parse(fs.readFileSync("./order.json"));
+
+    const sum = package.orders.reduce((summary, current) => {
+        return summary + (Number(current.price) * Number(current.number));
+    }, 0);
+
+    res.render("order", { orderList: package.orders, sum });
+});
+app.post("/myorder", function (req, res) {
     fs.readFile("./order.json", (err, data) => {
         if (err) {
             throw err;
-        }
-        else {
+        } else {
             const package = JSON.parse(data);
-            let newPackage = req.body;
-            // for (pizzaValue of package.orders) {
-            //     if (pizzaValue.pizza === newPackage.pizza) {
-            //         let pizzaTopUp = parseInt(pizzaValue.number) + parseInt(newPackage.number);
-            //         package.orders.number = pizzaTopUp
-            //     } if (pizzaValue.pizza !== newPackage.pizza) {
-            //     }
-            // }
-            package.orders.push(newPackage);
+            const orders = package.orders;
+            const newOrder = req.body;
+
+            const oldOrderIndex = orders.findIndex(
+                (order) => order.pizza === newOrder.pizza
+            );
+
+            if (oldOrderIndex > -1) {
+                orders[oldOrderIndex].number =
+                    Number(orders[oldOrderIndex].number) + Number(newOrder.number);
+            } else {
+                package.orders.push(newOrder);
+            }
             fs.writeFile("./order.json", JSON.stringify(package, null, 2), (err) => {
                 if (err) {
                     throw err;
-                }
-                else {
-                    res.redirect(301, "/pizza/list")
+                } else {
+                    res.redirect(301, "/pizza/list");
                 }
             });
-
         }
-    })
-})
+    });
+});
 app.post("/checkout", (req, res) => {
     const initObject = {
-        "orders": []
-    }
+        orders: [],
+    };
     fs.writeFile("./order.json", JSON.stringify(initObject, null, 2), (err) => {
         if (err) {
             throw err;
+        } else {
+            res.redirect(301, "/");
         }
-        else {
-            res.redirect(301, "/")
-        }
-    })
-})
+    });
+});
 
 const port = 9001;
 
-app.listen(port, _ => console.log(`http://127.0.0.1:${port}`));
+app.listen(port, (_) => console.log(`http://127.0.0.1:${port}`));
